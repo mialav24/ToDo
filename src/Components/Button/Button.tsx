1bab22ead491ac7a52833a92fe4f8d9b435de993
Michael <michael.alean@gmail.com>
2023-12-19 17:56:51 -0500

    Refactored and enhanced the Button component. Removed GridContainer

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,14 +1,27 @@
 import { ButtonStyled } from "./Button.styles";
 
 type ButtonProps = {
+  type: "submit" | "button";
   children: string;
   disabled?: boolean;
+  onClick?: () => void;
   variant?: "primary" | "secondary";
 };
 
-const Button = ({ children, variant, disabled }: ButtonProps) => {
+const Button = ({
+  children,
+  variant,
+  disabled,
+  onClick,
+  type,
+}: ButtonProps) => {
   return (
-    <ButtonStyled disabled={disabled} variant={variant}>
+    <ButtonStyled
+      disabled={disabled}
+      variant={variant}
+      onClick={onClick}
+      type={type}
+    >
       {children}
     </ButtonStyled>
   );
